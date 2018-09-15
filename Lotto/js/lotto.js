434e0d48c3c8d43d4a97dd11fb1c00d526164bc4
jyansol <hahn0406@gmail.com>
2018-09-16 01:15:09 +0900

    side_projec

--- Lotto/js/lotto.js
+++ Lotto/js/lotto.js
@@ -2,6 +2,7 @@
 
 let randomArr = [];
 const seletedNum = 6;
+const lottoPrice = 1000;
 
 const randomNum = () => {
   for (i = 0; i < seletedNum; i++) {
@@ -12,26 +13,14 @@ const randomNum = () => {
 randomNum();
 console.log(randomArr);
 
-const buyLotto = (money, lotto) => {
-  const autoNum = lotto / money;
+const buyLotto = (lottoPrice, buyLotto) => {
+  const autoNum = buyLotto / lottoPrice;
   for (i = 0; i < autoNum; i++) {
-    console.log(autoNum);
+    console.log(randomArr);
   }
 };
 
-buyLotto(1000, 14000);
+buyLotto(lottoPrice, 14000);
 
 //1000원일때 1개의 배열 생성
-
-// 01 . 새로운 배열 생성
-// 02. buyLottos(e)
-// 03. 6개의 숫자를 가진 배열이 e만큼 생성
-// 04. setLuckeyNumber(e)  : 배열로 생성
-// 05. 당첨통계
-// 05-1. 3개일치 - ? 개
-// 05-2. 4개일치 - ? 개
-// 05-3. 5개일치 - ? 개
-// 05-4. 6개일치 - ? 개
-// 06. 나의 수익률 계산
-
 // 0 이랑 중복숫자 안되게
