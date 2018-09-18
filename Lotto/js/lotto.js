a0ee0b50bb31a0e3c4350e05b11b89dce07ac6ea
jyansol <hahn0406@gmail.com>
2018-09-18 19:11:13 +0900

    My Side Project

--- Lotto/js/lotto.js
+++ Lotto/js/lotto.js
@@ -1,26 +1,45 @@
 //의미있는 숫자
 
 let randomArr = [];
+let luckyNum = [];
 const seletedNum = 6;
 const lottoPrice = 1000;
 
 const randomNum = () => {
   for (i = 0; i < seletedNum; i++) {
-    randomArr[i] = Math.floor(Math.random() * 45);
+    randomArr[i] = Math.floor(Math.random() * 44) + 1;
   }
 };
 
 randomNum();
-console.log(randomArr);
 
-const buyLotto = (lottoPrice, buyLotto) => {
-  const autoNum = buyLotto / lottoPrice;
-  for (i = 0; i < autoNum; i++) {
+// n번의 randomNum배열을 출력하는 함수 (1000원에 1줄)
+const buyLottos = (money) => {
+  const autoNum = money / lottoPrice;
+  for (j = 0; j < autoNum; j++) {
+    randomNum();
     console.log(randomArr);
   }
 };
 
-buyLotto(lottoPrice, 14000);
+buyLottos(14000);
 
-//1000원일때 1개의 배열 생성
-// 0 이랑 중복숫자 안되게
+// 중복숫자 안되게
+const notOverLap = () => {
+  for (j = 0; j < i; j++) {
+    if (randomArr[i] === randomArr[j]) {
+      i = i - 1;
+      break;
+    }
+  }
+};
+notOverLap();
+
+//오늘의 행운번호
+const setLuckyNum = () => {};
+
+setLuckyNum();
+
+console.log('dd');
+console.log(luckyNum);
+// []랑 []랑 같으면
