4008421bda2fde0e9be629bcefb6f517bfb571f6
jyansol <hahn0406@gmail.com>
2018-09-28 20:25:03 +0900

    side_project

--- constructor_practice/constructor_funtion.js
+++ constructor_practice/constructor_funtion.js
@@ -2,23 +2,39 @@
 ** 문제 1. 생성자
  */
 
-function Person(name, age, live) {
-  this.name = name;
-  this.age = age;
-  this.live = live;
-}
-
-// const Person = function (name, age, live) {
-
+// function Person(name, age, live) {
+//   this.name = name;
+//   this.age = age;
+//   this.live = live;
 // }
 
-Person.prototype.sayInfo = function() {
-  return this.name + '님이 사는 곳은' + this.live + '이며 나이는' + this.age + '세 입니다.';
-};
+// Person.prototype.sayInfo = function() {
+//   return this.name + '님이 사는 곳은' + this.live + '이며 나이는' + this.age + '세 입니다.';
+// };
 
-const person = new Person('김김이', 29, '서울');
-const person2 = new Person('박박이', 30, '부산');
+// const person = new Person('김김이', 29, '서울');
+// const person2 = new Person('박박이', 30, '부산');
 
+// console.log(person.sayInfo());
+// console.log(person2.sayInfo());
+
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+// ES2015에서 도입된 클래스는 생성자의 기능을 대체합니다.
+// class 표현식을 사용하면, 생성자와 같은 기능을 하는 함수를 훨씬 더 깔끔한 문법으로 정의할 수 있습니다.
+// class는 함수가 아닙니다!
+// 객체 => 생성자 => 프로토타입 => Class
+class Person {
+  constructor({ name, age, live }) {
+    this.name = name;
+    this.age = age;
+    this.live = live;
+  }
+  sayInfo() {
+    return `${this.name}님이 사는 곳은 ${this.live}이며, 나이는${this.age}세 입니다.`;
+  }
+}
+const person = new Person({ name: '김김이', age: 29, live: '서울' });
+const person2 = new Person({ name: '박박이', age: 29, live: '부산' });
 console.log(person.sayInfo());
 console.log(person2.sayInfo());
 
--- constructor_practice/constructor_test.js
+++ constructor_practice/constructor_test.js
@@ -1,22 +1,22 @@
-const myObj1 = {
-  car: 'Lambo',
-};
+// const myObj1 = {
+//   car: 'Lambo',
+// };
 
-const myObj3 = {
-  name: 'Amie',
-  age: 25,
-  favLanguage: 'Rails',
-};
+// const myObj3 = {
+//   name: 'Amie',
+//   age: 25,
+//   favLanguage: 'Rails',
+// };
 
-Object.assign(myObj1, myObj3);
-//clone
-console.log(myObj1);
+// Object.assign(myObj1, myObj3);
+// //clone
+// console.log(myObj1);
 
-console.log(Object.entries(myObj3));
-//key val
+// console.log(Object.entries(myObj3));
+// //key val
 
-const myObj = Object.getOwnPropertyDescriptor(myObj3);
-console.log(myObj3);
+// const myObj = Object.getOwnPropertyDescriptor(myObj3);
+// console.log(myObj3);
 
 // .definePropertier() / seal
 
@@ -34,40 +34,69 @@ console.log(myObj3);
 // entries(), keys(), values()
 // dd['vv[0]']
 
-// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-
-function Person(n, a) {
-  this.name = n;
-  this.age = a;
-} //생성자 함수 : 대문자로 시작 , this - 인스턴스 , 객체를 만들고 초기화(setting)
-
-Person.prototype.setOlder = function() {
-  this.age += 1;
-};
-
-Person.prototype.getAge = function() {
-  return this.age;
-};
-//prototype
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//생성자 함수 : 대문자로 시작 , this - 인스턴스 , 객체를 만들고 초기화(setting)
 //ex) array.filter(),map()이 되는 이유도 array.prototype
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
 
-const gomu = new Person('고무곰', 30);
-const iu = new Person('아이유', 25);
+// Person.prototype.setOlder = function() {
+//   this.age += 1;
+// };
 
-// ---------------------------------------
+// Person.prototype.getAge = function() {
+//   return this.age;
+// };
 
-function Family(n, a) {
-  this.name = n;
-  this.age = a;
+// const gomu = new Person('고무곰', 30);
+// const iu = new Person('아이유', 25);
+// console.log(gomu.setOlder());
+// console.log(gomu.setOlder());
+// console.log(gomu.setOlder());
+// console.log(gomu.getAge());
+
+//class로 바꾸기
+class Person {
+  constructor({ name, age }) {
+    this.name = name;
+    this.age = age;
+  }
+  setOlder() {
+    this.age += 1;
+    return this.age;
+  }
 }
+const person = new Person({ name: '고무곰', age: 30 });
+console.log(person.setOlder());
 
-Family.prototype.sayName = function() {
-  return this.name;
-};
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+// String.prototype.myMethod = String에 부여할 수 있음
 
-const me = new Family('쟌솔', 29);
-const pika = new Family('피카', 4);
+// function Family(n, a) {
+//   this.name = n;
+//   this.age = a;
+// }
+// Family.prototype.sayName = function() {
+//   return this.name;
+// };
+// const me = new Family('쟌솔', 29);
+// const pika = new Family('피카', 4);
+
+// console.log(me.sayName());
+
+//class로 바꾸기
+class Family {
+  constructor({ name, age }) {
+    this.name = name;
+    this.age = age;
+  }
+  sayName() {
+    return this.name;
+  }
+}
+const me = new Family({ name: '쟌솔', age: 29 });
+const pika = new Family({ name: '피카', age: 6 });
 
 console.log(me.sayName());
-
-// String.prototype.myMethod = String에 부여할 수 있음
