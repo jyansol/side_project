bbb35f303f58fde518cbbec11b97c4870235a0b4
jyansol <hahn0406@gmail.com>
2018-12-09 12:51:36 +0900

    React Route 연습

--- my-router/src/App.js
+++ my-router/src/App.js
@@ -1,28 +1,105 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import { Link, Route, Switch } from 'react-router-dom';
+// import './App.css';
 
-class App extends Component {
+// const Home = () => {
+//   <div>
+//     <h2>Home</h2>
+//   </div>;
+// };
+
+// const Category = () => {
+//   <div>
+//     <h2>Category</h2>
+//   </div>;
+// };
+
+// const Products = () => {
+//   <div>
+//     <h2>Products</h2>
+//   </div>;
+// };
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <nav>
+//           <ul className="nav">
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/category">Category</Link>
+//             </li>
+//             <li>
+//               <Link to="/products">Products</Link>
+//             </li>
+//           </ul>
+//         </nav>
+//         <Route path="/" component={Home} />
+//         <Route path="/category" />
+//         <Route path="/products" />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+
+/* Home component */
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+/* Category component */
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+);
+
+/* Products component */
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+);
+
+/* App component */
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <nav className="navbar navbar-light">
+          <ul className="nav navbar-nav">
+            <li>
+              <Link to="/">Homes</Link>
+            </li>
+            <li>
+              <Link to="/category">Category</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" component={Home} />
+        <Route path="/category" component={Category} />
+        <Route path="/products" component={Products} />
       </div>
     );
   }
 }
+
+//Route dom에 path가 들어오면 component를 반환
+//Link는 표시줄을 바꿔주고, Route는 컴포넌트를 불러옴
+//전체를 감싸는 BrowserRouter
+//SPA!
 
 export default App;
